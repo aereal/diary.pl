bd22c629e390da2232b54c074b2ee07485202153
aereal <aereal@kerare.org>
2012-03-28 17:06:26 +0900

    Prettify diary.pl list outputs

--- lib/InternDiary/App/CLI/List.pm
+++ lib/InternDiary/App/CLI/List.pm
@@ -2,6 +2,7 @@ package InternDiary::App::CLI::List;
 use strict;
 use warnings;
 use parent qw/CLI::Dispatch::Command/;
+use List::MoreUtils qw/apply/;
 
 use InternDiary::App;
 
@@ -13,7 +14,7 @@ sub app {
 sub run {
     my ($self) = @_;
     for (@{$self->app->list_entries}){
-        print sprintf "%s: %s\n", $_->created_at->iso8601, $_->title;
+        print sprintf "ID:%s %s (%s)\n", $_->id, apply { s/\n+// } $_->title, $_->created_at->iso8601;
     }
 }
 
