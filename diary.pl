f7f78c0a035d9b092234ff68c5063963138f478d
aereal <aereal@kerare.org>
2012-03-28 15:36:14 +0900

    Add diary.pl new command

---
+++ diary.pl
@@ -0,0 +1,8 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use lib 'lib';
+use CLI::Dispatch;
+
+CLI::Dispatch->run('InternDiary::App::CLI');
--- lib/InternDiary/App/CLI/New.pm
+++ lib/InternDiary/App/CLI/New.pm
@@ -2,5 +2,27 @@ package InternDiary::App::CLI::New;
 use strict;
 use warnings;
 use parent qw/CLI::Dispatch::Command/;
+use File::Temp;
+use Path::Class qw/file/;
+
+use InternDiary::App;
+
+sub app {
+    my ($self) = @_;
+    $self->{app} ||= InternDiary::App->new;
+}
+
+sub run {
+    my ($self, @args) = @_;
+    my $editor = $ENV{EDITOR} or die '$EDITOR is not set';
+    my $tempfile = File::Temp->new;
+    $self->log(debug => "\$EDITOR is $editor");
+    $self->log(debug => '$tempfile->filename is ' . $tempfile->filename);
+    system $editor, $tempfile->filename;
+    close $tempfile;
+    my $entry = $self->app->create_entry(file($tempfile->filename)->slurp);
+    print $entry ? 'Successfully created entry!' : 'Failed to create entry';
+    print "\n"
+}
 
 1;
