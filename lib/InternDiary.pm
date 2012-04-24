4a66fb81e1a988e2f65f942b05098052bb6cc04f
aereal <aereal@kerare.org>
2012-04-24 12:21:13 +0900

    Add #current_user method (temporary implementation)

--- lib/InternDiary.pm
+++ lib/InternDiary.pm
@@ -2,7 +2,13 @@ package InternDiary;
 use strict;
 use warnings;
 use parent qw/Ridge/;
+use InternDiary::MoCo::User;
 
 __PACKAGE__->configure;
+
+sub current_user {
+    my ($self) = @_;
+    InternDiary::MoCo::User->search->first;
+}
 
 1;
