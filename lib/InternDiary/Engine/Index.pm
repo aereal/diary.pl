569ab54a977c1cad1547c87b05505e1720d208de
aereal <aereal@kerare.org>
2012-04-24 18:35:38 +0900

    Paginate entries

--- lib/InternDiary/Engine/Index.pm
+++ lib/InternDiary/Engine/Index.pm
@@ -4,12 +4,12 @@ use warnings;
 use InternDiary::Engine -Base;
 use InternDiary::App;
 use InternDiary::MoCo::User;
-#use InternDiary::MoCo::Entry;
+use InternDiary::MoCo::Entry;
 
 sub default : Public {
     my ($self, $r) = @_;
     $r->stash->param(
-        entries => $r->current_user->entries->to_a
+        entries => InternDiary::MoCo::Entry->page($r->req->param('page'))->to_a
     );
 }
 
