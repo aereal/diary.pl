02e37b47da09acd6ce321a6fe3e58f18950998be
aereal <aereal@kerare.org>
2012-04-24 16:57:21 +0900

    Add POST /entry (create) action

--- lib/InternDiary/Engine/Entry.pm
+++ lib/InternDiary/Engine/Entry.pm
@@ -7,11 +7,36 @@ use HTTP::Status;
 
 sub default : Public {
     my ($self, $r) = @_;
+    $r->follow_method;
+}
+
+sub _get {
+    my ($self, $r) = @_;
     my $entry = InternDiary::MoCo::Entry->retrieve($r->req->param('id'))
         or Ridge::Exception::RequestError->throw(code => RC_NOT_FOUND);
     $r->stash->param(
         entry => $entry
     );
+}
+
+sub _post {
+    my ($self, $r) = @_;
+
+    $r->req->form(
+        title => ['NOT_BLANK', 'ASCII'],
+        body => ['NOT_BLANK', 'ASCII']
+    );
+
+    if ($r->req->form->has_error) {
+        # TODO
+    } else {
+        my $entry = $r->current_user->create_entry({
+            title => $r->req->param('entry_title'),
+            body => $r->req->param('entry_body'),
+        });
+
+        $r->res->redirect($r->entry_path(id => $entry->id));
+    }
 }
 
 sub new_ : Public {
