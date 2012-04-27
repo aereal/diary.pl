534a1c95dbf734fb9e8dc10bff7020e8fa23df5f
aereal <aereal@kerare.org>
2012-04-27 11:12:52 +0900

    Add pagination links

--- lib/InternDiary/Engine/Index.pm
+++ lib/InternDiary/Engine/Index.pm
@@ -10,7 +10,7 @@ use InternDiary::MoCo::Entry;
 sub default : Public {
     my ($self, $r) = @_;
     my $model = 'InternDiary::MoCo::Entry';
-    my $pager = $r->pager($model, $r->req->param('page') || 1, 1);
+    my $pager = $r->pager($model, $r->req->param('page') || 1, $r->config->{app_config}->{default}->{per_page});
     $r->stash->param(
         entries => $r->paginate($model, $pager)->to_a,
         pager => $pager,
